f3a48acde30db27f267b9cf4d66773ae4702e74f
DevOps DevOps <fredypinto@cbit-online.com>
2020-08-06 16:30:36 -0500

    linea base

---
+++ springboot-servicio-eurek-server/src/main/java/com/formaciondbi/springboot/app/eureka/SpringbootServicioEurekServerApplication.java
@@ -0,0 +1,15 @@
+package com.formaciondbi.springboot.app.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringbootServicioEurekServerApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootServicioEurekServerApplication.class, args);
+	}
+
+}
